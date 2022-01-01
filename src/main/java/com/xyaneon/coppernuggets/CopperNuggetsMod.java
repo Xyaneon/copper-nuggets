822dbd63cbc143a43080075e29585cf07a85b8ee
Christopher Kyle Horton <christhehorton@gmail.com>
2022-01-01 02:19:16 -0500

    Clean up unneeded comments and mixin code

--- src/main/java/com/xyaneon/coppernuggets/CopperNuggetsMod.java
+++ src/main/java/com/xyaneon/coppernuggets/CopperNuggetsMod.java
@@ -11,19 +11,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class CopperNuggetsMod implements ModInitializer {
-    // This logger is used to write text to the console and the log file.
-    // It is considered best practice to use your mod id as the logger's name.
-    // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LogManager.getLogger("coppernuggets");
 
     public static final Item COPPER_NUGGET = new CopperNugget(new FabricItemSettings().group(ItemGroup.MISC));
 
     @Override
     public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-
         LOGGER.info("Copper Nuggets mod initialized.");
 
         Registry.register(Registry.ITEM, new Identifier("coppernuggets", "copper_nugget"), COPPER_NUGGET);
