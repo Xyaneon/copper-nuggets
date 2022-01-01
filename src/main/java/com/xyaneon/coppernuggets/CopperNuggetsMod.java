00cff649ab7b967883397a5f5d724c4fd855f9ee
Christopher Kyle Horton <christhehorton@gmail.com>
2021-12-31 20:07:06 -0500

    Create separate CopperNugget class

--- src/main/java/com/xyaneon/coppernuggets/CopperNuggetsMod.java
+++ src/main/java/com/xyaneon/coppernuggets/CopperNuggetsMod.java
@@ -1,5 +1,6 @@
 package com.xyaneon.coppernuggets;
 
+import com.xyaneon.coppernuggets.items.CopperNugget;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
@@ -10,21 +11,21 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class CopperNuggetsMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("coppernuggets");
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LogManager.getLogger("coppernuggets");
 
-	public static final Item FABRIC_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item COPPER_NUGGET = new CopperNugget(new FabricItemSettings().group(ItemGroup.MISC));
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
 
-		LOGGER.info("Copper Nuggets mod initialized.");
+        LOGGER.info("Copper Nuggets mod initialized.");
 
-		Registry.register(Registry.ITEM, new Identifier("coppernuggets", "copper_nugget"), FABRIC_ITEM);
-	}
+        Registry.register(Registry.ITEM, new Identifier("coppernuggets", "copper_nugget"), COPPER_NUGGET);
+    }
 }
